b92eee771fda5a08dca5df3d18de233f9b9939b2
WINDOWS 10 <WINDOWS 10@Windows10>
2018-08-03 11:01:56 +0700

    Coba IF

---
+++ src/jobsheet7/latihan.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class latihan {
+    public static void main (String [] args) {
+        String identitas = "Rullyana Mahirotul Aisya / XRPL5 / 38";
+        System.out.println("Identitas : "+ identitas);
+        
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Nilai a ? ");
+        int a = scanner.nextInt();
+        
+        System.out.println("\nIF.. 1");
+        if(a < 5) System.out.println("nilai a kurang dari 5");
+        
+        System.out.println("\nIF.. 2");
+        if(a == 5)
+            System.out.println("nilai a sama dengan 5");
+        System.out.println("\nIF.. 3");
+        if(a > 5) {
+            System.out.println("isi variable a :"+ a);
+            System.out.println("nilai a lebih dari 5");
+        }
+    }
+}
